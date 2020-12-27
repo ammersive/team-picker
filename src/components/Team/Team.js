6a00551de922e6d7bb61df55c450365641764a2d
Sophie Stammers <sophie.stammers@gmail.com>
2020-12-27 10:47:50 +0000

    Styling: Fixed widths and colours in Game component

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,6 +1,6 @@
 const Team = ({ teamName, team, generateClick }) => {
   return (
-    <>
+    <div className="team-wrapper">
       <h3 className="team-name">{ teamName }</h3>
       <button className="generate" onClick={ generateClick }>
         {teamName.substring(0 , 4) === "Team" ? "Generate team name" : "New name"}
@@ -10,7 +10,7 @@ const Team = ({ teamName, team, generateClick }) => {
           <li className="team-member" key={ index }>{ player.name }</li> 
         )) } 
       </ul>
-    </>
+    </div>
   );
 };
 
