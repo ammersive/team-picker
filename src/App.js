33742d411a99789ca30c95c9a7293f292db6efcc
Sophie Stammers <sophie.stammers@gmail.com>
2020-12-28 15:12:43 +0000

    Component: About template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import About from "./components/About";
 import NewPlayers from "./components/NewPlayers";
 import Teams from "./components/Teams";
 
 const App = () => (
   <>
     <Header>Heading</Header>
+    <About />
     <NewPlayers />
     <Teams />
   </>
---
+++ src/components/About.js
@@ -0,0 +1,32 @@
+const About = () => (
+  <section className="section-wrapper">
+    <h2 className="section-header">About</h2>
+    <div className="overview-wrapper">
+      <div className="about-box overview">
+        <h3 className="about-subheading">About overview</h3>
+        <p className="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+        <p className="about-text">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+      </div>
+    </div>
+    <div className="about-boxes">
+      <div className="about-box">
+        <h3 className="about-subheading">Player bank</h3>
+        <p className="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>      
+      <div className="about-box">
+        <h3 className="about-subheading">Draw by play-count</h3>
+        <p className="about-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        
+      </div>      
+      <div className="about-box">
+        <h3 className="about-subheading">Randomise Teams</h3>
+        <p className="about-text">And shuffle them until you're happy. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. </p>
+      </div>
+      <div className="about-box">
+        <h3 className="about-subheading">Team name generator</h3>
+        <p className="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod </p>
+      </div>
+    </div>
+  </section>
+);
+export default About;
